9c63fdf7d00bd25112d233cc0b7526feb4a4c57c
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2014-10-03 20:29:57 +0200

    Added all the other nom package.json objects

--- package.json.rb
+++ package.json.rb
@@ -13,6 +13,8 @@ class Package
   #  puts "Hi #{@file}!"
   #end
 
+  # https://www.npmjs.org/doc/files/package.json.html#name
+  # @return [String] the package.json name value
   def name
     @data['name']
   end
@@ -29,8 +31,44 @@ class Package
     @data['keywords']
   end
 
+  def homepage
+    @data['homepage']
+  end
+
+  def bugs
+    @data['bugs']
+  end
+
+  def license
+    @data['license']
+  end
+
   def author
     @data['author']
+  end
+
+  def contributors
+    @data['contributors']
+  end
+
+  def files
+    @data['files']
+  end
+
+  def main
+    @data['main']
+  end
+
+  def bin
+    @data['bin']
+  end
+
+  def man
+    @data['man']
+  end
+
+  def directories
+    @data['directories']
   end
 
   def repository
@@ -41,12 +79,56 @@ class Package
     @data['scripts']
   end
 
+  def config
+    @data['config']
+  end
+
   def dependencies
     @data['dependencies']
   end
 
   def devDependencies
     @data['devDependencies']
+  end
+
+  def peerDependencies
+    @data['peerDependencies']
+  end
+
+  def bundledDependencies
+    @data['bundledDependencies']
+  end
+
+  def optionalDependencies
+    @data['optionalDependencies']
+  end
+
+  def engines
+    @data['engines']
+  end
+
+  def engineStrict
+    @data['engineStrict']
+  end
+
+  def os
+    @data['os']
+  end
+
+  def cpu
+    @data['cpu']
+  end
+
+  def preferGlobal
+    @data['preferGlobal']
+  end
+
+  def private
+    @data['private']
+  end
+
+  def publishConfig
+    @data['publishConfig']
   end
 
 end
